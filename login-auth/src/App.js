a2f3ba0dfab17b3801dd2387663ec6cb83a9b31f
Harshkumar dudhat <harshp.dudhat9998@gmail.com>
2024-03-22 15:30:57 +0530

    some chang

--- login-auth/src/App.js
+++ login-auth/src/App.js
@@ -1,48 +1,8 @@
-// import React, { useEffect, useState } from 'react';
-// import "./App.css";
-// import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-// import CreatePage from "./CreatePage/CreatePage";
-// import DashboardData from "./Dashboard/Dashboard";
-// import Loginpage from "./CreatePage/Loginpage";
-// import Note from "./Dashboard/Notes";
-// import NoPage from "./componets/404/index"
-
-// function App() {
-//   const [isLogin, setIsLogin] = useState(false);
-//   useEffect(() => {
-//     if (localStorage.getItem("token")) {
-//       setIsLogin(true)
-//     }
-//   }, []);
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Navigate to="/createAc" />} />
-//           <Route path="/createAc" element={isLogin ? <Navigate to="/dashbord" /> : <CreatePage />} />
-//           <Route path="/login" element={isLogin ? <Navigate to="/dashbord" /> : <Loginpage />} />
-
-//           <Route path="/dashbord" element={!isLogin ? <Navigate to="/login" /> : <DashboardData setIsLogin={setIsLogin} />} />
-//           <Route path="/notes" element={!isLogin ? <Navigate to="/login" /> : <Note />} />
-
-//           <Route path="*" element={<NoPage />} />
-
-//         </Routes>
-//       </BrowserRouter>
-//     </>);
-// }
-
-
-
-
-// export default App;
-
-// Import necessary components and libraries
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import CreatePage from './CreatePage/CreatePage';
-import DashboardData from './Dashboard/Dashboard';
 import Loginpage from './CreatePage/Loginpage';
+import DashboardData from './Dashboard/Dashboard';
 import Note from './Dashboard/Notes';
 import NoPage from './componets/404/index';
 
